527aaff0805dac60291cb197f0638ec8838d7cc8
Yan <yanman1986@gmail.com>
2024-02-02 23:34:34 -0500

    added code interpreter via assistant api, to write the transform_2d_array funciton, and check its implementation validity against the training dataset.

--- arc_gpt_predictor.py
+++ arc_gpt_predictor.py
@@ -5,20 +5,24 @@ import numpy as np
 import os
 import copy
 import csv
+import time
 from typing import List
 from pydantic import BaseModel, Field
 
 # abstraction class to facilitate message array management and prompt loading
-class ARCOpenAISolver:
-    def __init__(self):
-        with open('sys_prompt.txt', 'r') as file:
+class BaseOpenAIChatApp:
+    def __init__(self, sys_prompt_file, output_format_file):
+        with open(sys_prompt_file, 'r') as file:
             self.sys_prompt = file.read()
-        self.output_format_prompt = "Do not include any explanations, only provide a RFC8259 compliant JSON response following this format without deviation.\n"
-        self.output_format_prompt += "{'output': <2D array representing a rectangular grid of integers ranging from 0 to 9>}"
+        with open(output_format_file, 'r') as file:
+            self.output_format_prompt = file.read()
         self.num_tries = 3
         api_key = os.getenv('OPENAI_API_KEY')
         self.client = OpenAI(api_key=api_key)
         self.reset()
+
+    def reset(self):
+        self.messages = [{"role": "system", "content": self.sys_prompt}]
 
     # function to send a chat message and manage the conversation message queues
     def chat(self, user_prompt):
@@ -30,27 +34,9 @@ class ARCOpenAISolver:
 
         self.messages= [{"role": "assistant", "content": response.choices[0].message.content}]
         return response
-    
-    # function to reset the conversation
-    def reset(self):
-        self.messages = [{"role": "system", "content": self.sys_prompt}]
-
-    # utility function to check if the output json object matches with the expected schema
-    def schema_match(self, json_obj):
-        if "output" not in json_obj:
-            return False
-        if not isinstance(json_obj["output"], list) or not all(isinstance(row, list) for row in json_obj["output"]):
-            return False
-        if len(json_obj["output"]) == 0:
-            return False
-        row_length = len(json_obj["output"][0])
-        for row in json_obj["output"]:
-            if len(row) != row_length or not all(isinstance(element, int) for element in row):
-                return False
-        return True
 
     # extract the strict json object from the detailed answer given by GPT 
-    def strict_json(self):
+    def strict_output(self):
         complain = ""
         for i in range(self.num_tries):
             strict_json_prompt = complain + self.output_format_prompt
@@ -63,15 +49,59 @@ class ARCOpenAISolver:
                 messages=self.messages)
             try:
                 response_content = json.loads(response.choices[0].message.content)
-                print(response_content)
-                if not self.schema_match(response_content):
-                    raise json.JSONDecodeError("The JSON schema of the response does not match the expected schema.")
                 return response_content
             except json.JSONDecodeError:
                 print("{}th attempt at decoding for strict JSON output")
                 complain = "The JSON returned is not valid, please "
+  
+class CodeTester(BaseOpenAIChatApp):
+    # function to send a chat message and manage the conversation message queues
+    def chat(self, user_prompt, file_name, assistant_timeout=600):
+        file = self.client.files.create(
+            file=open(file_name, "rb"),
+            purpose='assistants'
+        )
+        self.messages = [{"role": "user", "content": user_prompt, "file_ids": [file.id]}]
+        #assistant = self.client.beta.assistants.create(
+        #    instructions=self.sys_prompt,
+        #    model="gpt-4-0125-preview",
+        #    tools=[{"type": "code_interpreter"}],
+        #)
+        assistant = self.client.beta.assistants.retrieve(assistant_id="asst_qBOR5eFp7Wz5RRCgblO5QfwB")
+        thread = self.client.beta.threads.create(
+            messages=self.messages
+        )
+        run = self.client.beta.threads.runs.create(
+          thread_id=thread.id,
+          assistant_id=assistant.id
+        )
 
-chatApp = ARCOpenAISolver()
+        timeout = 0
+        print("Awaiting on Assistant thread to finish")
+        while timeout < assistant_timeout:
+            run = self.client.beta.threads.runs.retrieve(thread_id=thread.id, run_id=run.id)
+            if run.status == "completed":
+                break
+            
+            time.sleep(1)
+            timeout = timeout + 1
+        
+        run_steps = self.client.beta.threads.runs.steps.list(
+            thread_id=thread.id,
+            run_id=run.id
+        )
+
+        for step_data in run_steps.data:
+            if step_data.status == "completed" and step_data.step_details.type == "tool_calls":
+                output = step_data.step_details.tool_calls[0].code_interpreter.outputs[0].logs
+                print("code_intepreter output")
+                print(output)
+                self.messages= [{"role": "assistant", "content": output}]
+                return output
+
+
+solver = BaseOpenAIChatApp(sys_prompt_file="sys_prompt.txt", output_format_file="output_format.txt")
+code_tester = CodeTester(sys_prompt_file="code_tester_sys_prompt.txt", output_format_file="code_output_format.txt")
 
 # helper function to convert grids from 2D array format to the submission format
 def flattener(pred):
@@ -83,19 +113,22 @@ def flattener(pred):
     return str_pred
 
 # a function that feeds the ARCOpenAISolver the task information and the output id and
-# return the flattened representation of the output
-def solve_arc_question(task_json, output_id):
-    chatApp.reset()
+# return the output
+def solve_arc_question(task_json, task_file_path, output_id):
+    solver.reset()
     modified_task_json = copy.deepcopy(task_json)
     modified_task_json["test"][output_id]["output"] = "'to_be_filled'"
     user_prompt = "Here is a JSON of the input-output pairs\n{}".format(modified_task_json)
-    verbose_response = chatApp.chat(user_prompt=user_prompt)
+    verbose_response = solver.chat(user_prompt=user_prompt)
     print(verbose_response.choices[0].message.content)
-    
-    response = chatApp.strict_json()
+    response = solver.strict_output()
 
-    output = response["output"]
-    return output 
+    code_tester.reset()
+    code_tester_prompt = "The task json is attached.\n The output_id is {}\nHere is the description of the relationship between input and output 2D grids:\n{}\n".format(output_id, response)
+    code_response = code_tester.chat(user_prompt=code_tester_prompt, file_name=task_file_path)
+    code_response = code_tester.strict_output()
+    print(code_response)
+    return code_response 
         
 # helper function to plot and visualize the input/output pairs in train set
 def show_image_from_json(task_json, input, predicted, groundtruth):
@@ -116,6 +149,7 @@ def show_image_from_json(task_json, input, predicted, groundtruth):
     axs[-2, 0].imshow(input_image, cmap='viridis')
     axs[-2, 1].set_title('Predicted')
     axs[-2, 1].imshow(predicted_image, cmap='viridis')
+
     if groundtruth:
       groundtruth_image = np.array(groundtruth)
       axs[-1, 0].set_title('Groundtruth')
@@ -129,39 +163,44 @@ if __name__ == "__main__":
     #train_files = os.listdir(folder)
     folder = './evaluation/'
     task_files = os.listdir(folder)
+    retries = 3
 
     output_ids = []
     outputs = []
     for json_file in task_files:
-        with open(os.path.join(folder, json_file), 'r') as f:
+        task_file_path = os.path.join(folder, json_file)
+        with open(task_file_path, 'r') as f:
             task_data = json.load(f)
             task_id = json_file.split('.')[0]
 
-            # for each question within each task, append a flattened output
-            # with the corresponding {task_id}_{output_id}
-            for output_id, question in enumerate(task_data["test"]):
-                print("solving for task {} output_id {}".format(task_id, output_id))
+        # for each question within each task, append a flattened output
+        # with the corresponding {task_id}_{output_id}
+        for output_id, question in enumerate(task_data["test"]):
+            print("solving for task {} output_id {}".format(task_id, output_id))
+            output = [[0]]
+            for i in range(retries):
                 try:
-                    output = solve_arc_question(task_data, output_id)
+                    solution = solve_arc_question(task_data, task_file_path, output_id)
+                    if solution["passed"]:
+                        output = solution["output"]
+                        break
                 except Exception as err:
                     print("Error encountered during answering for task {}, error: {}".format(task_id, err))
-                    output = [[0]]
-                    #raise err
-                groundtruth = None
-                if "output" in task_data["test"][output_id]:
-                    groundtruth = task_data["test"][output_id]["output"]
-                    print("predicted {} vs groundtrudth {}".format(flattener(output), flattener(groundtruth)))
-                    print("Pass? {}".format(flattener(output) == flattener(groundtruth)))
-                outputs.append(output)
-                output_ids.append("{}_{}".format(task_id, output_id))
+                
+            groundtruth = None
+            if "output" in task_data["test"][output_id]:
+                groundtruth = task_data["test"][output_id]["output"]
+                print("predicted {} vs groundtruth {}".format(flattener(output), flattener(groundtruth)))
+                print("Pass? {}".format(flattener(output) == flattener(groundtruth)))
+            outputs.append(output)
+            output_ids.append("{}_{}".format(task_id, output_id))
 
-                test_input = task_data["test"][output_id]["input"]
-                show_image_from_json(task_data, test_input, output, groundtruth)
+            test_input = task_data["test"][output_id]["input"]
+            show_image_from_json(task_data, test_input, output, groundtruth)
 
-
-            # write out the submission.csv according to ARC requirement
-            with open('submission.csv', 'w', newline='') as file:
-                writer = csv.writer(file)
-                writer.writerow(["output_id", "output"])
-                for i in range(len(output_ids)):
-                    writer.writerow([output_ids[i], flattener(outputs[i])])
+        # write out the submission.csv according to ARC requirement
+        with open('submission.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(["output_id", "output"])
+            for i in range(len(output_ids)):
+                writer.writerow([output_ids[i], flattener(outputs[i])])
